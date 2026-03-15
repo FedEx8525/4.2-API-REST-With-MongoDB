1a459c0a93158a62185a2c91fab45dc56645b56f
Federico Cantore <f.cantore85@gmail.com>
2026-03-15 16:11:03 +0100

    feat: add OrderController with creatOrder POST endpoint

--- src/main/java/cat/itacademy/s04/t02/n03/fruit_order_api/controller/OrderController.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit_order_api/controller/OrderController.java
@@ -1,4 +1,28 @@
 package cat.itacademy.s04.t02.n03.fruit_order_api.controller;
 
+import cat.itacademy.s04.t02.n03.fruit_order_api.dto.OrderRequestDTO;
+import cat.itacademy.s04.t02.n03.fruit_order_api.dto.OrderResponseDTO;
+import cat.itacademy.s04.t02.n03.fruit_order_api.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDTO> createOrder(@Valid @RequestBody OrderRequestDTO orderRequestDTO) {
+        OrderResponseDTO savedOrder = orderService.createOrder(orderRequestDTO);
+        return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
 }
